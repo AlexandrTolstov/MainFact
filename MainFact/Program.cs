14aea4de021963a094682caac5db032a6eb80222
AlexandrTolstov <alextav_85@mail.ru>
2019-01-06 13:00:26 +0600

    Add project files.

---
+++ MainFact/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainFact
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
